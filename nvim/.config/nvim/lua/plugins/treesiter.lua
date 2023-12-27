cf933574f18ed1a09b36266f9296483a47dc5dcf
Luis Zelaya <codewithluis11@gmail.com>
2023-12-26 22:42:04 -0600

    Deleting files from older config

--- nvim/.config/nvim/lua/plugins/treesiter.lua
+++ nvim/.config/nvim/lua/plugins/treesiter.lua
@@ -59,6 +59,24 @@ return {
                     ["il"] = "@loop.inner"
                 },
             }
+        },
+        playground = {
+            enable = true,
+            disable = {},
+            updatetime = 25,
+            persist_queries = false,
+            keybindings = {
+                toggle_query_editor = 'o',
+                toggle_hl_groups = 'i',
+                toggle_injected_languages = 't',
+                toggle_anonymous_nodes = 'a',
+                toggle_language_display = 'I',
+                focus_language = 'f',
+                unfocus_language = 'F',
+                update = 'R',
+                goto_node = '<cr>',
+                show_help = '?'
+            }
         }
     },
     config = function(_, opts)
@@ -72,6 +90,7 @@ return {
                 return true
             end, opts.ensure_installed)
         end
+
         require("nvim-treesitter.configs").setup(opts)
     end,
 }
