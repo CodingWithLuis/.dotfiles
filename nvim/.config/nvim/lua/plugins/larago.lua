9769b7f231d525ad3021c3b501eb3ff158919eb2
Luis Zelaya <codewithluis11@gmail.com>
2024-01-06 08:18:45 -0600

    Add Larago plugin

---
+++ nvim/.config/nvim/lua/plugins/larago.lua
@@ -0,0 +1,9 @@
+return {
+    'ccaglak/larago.nvim',
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+    keys = {
+        { "<leader>gg", "<cmd>GoBlade<cr>", mode = "n", desc = "Go to Blade View" }
+    }
+}
