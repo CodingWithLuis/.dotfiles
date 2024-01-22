cac8168ff077eff9e67727085340652b0b578642
Luis Zelaya <codewithluis11@gmail.com>
2024-01-21 23:32:21 -0600

    Installed and Enable FZF Telescope Extension

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,10 +6,14 @@ return
     dependencies = {
         {
             'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope-fzf-native.nvim',
             build = "make",
             enabled = vim.fn.executable("make") == 1,
         },
+        {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build =
+            'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+        }
     },
     keys = {
         {
@@ -110,6 +114,14 @@ return
                     },
                 },
             },
+            extensions = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case",
+                }
+            }
         }
     end,
 }
