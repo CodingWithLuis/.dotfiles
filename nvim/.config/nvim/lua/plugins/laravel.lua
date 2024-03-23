bc9b0f27f4d88aed455d037731793a76631c7a2f
Luis Zelaya <codewithluis11@gmail.com>
2024-03-22 18:28:11 -0600

    Disable none-ls on Laravel plugin

--- nvim/.config/nvim/lua/plugins/laravel.lua
+++ nvim/.config/nvim/lua/plugins/laravel.lua
@@ -4,14 +4,22 @@ return {
         "nvim-telescope/telescope.nvim",
         "tpope/vim-dotenv",
         "MunifTanjim/nui.nvim",
-        "nvimtools/none-ls.nvim"
+        "nvimtools/none-ls.nvim",
     },
     cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
     keys = {
         { "<leader>la", ":Laravel artisan<cr>" },
         { "<leader>lr", ":Laravel routes<cr>" },
         { "<leader>lm", ":Laravel related<cr>" },
+        { "<leader>lc", ":LaravelMyCommands<cr>" },
     },
     event = { "VeryLazy" },
+    opts = {
+        features = {
+            null_ls = {
+                enable = false,
+            }
+        },
+    },
     config = true,
 }
