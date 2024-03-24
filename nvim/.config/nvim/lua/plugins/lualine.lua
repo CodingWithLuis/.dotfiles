de3e21b40143c7f0c61785dd6d5a5e405853544d
Luis Zelaya <codewithluis11@gmail.com>
2024-03-24 17:48:12 -0600

    Change lualine theme

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
             require('lualine').setup({
                 options = {
                     icons_enabled = true,
-                    theme = 'solarized_dark',
+                    theme = 'powerline',
                     component_separators = { left = '', right = '' },
                     section_separators = { left = '', right = '' },
                     disabled_filetypes = {
