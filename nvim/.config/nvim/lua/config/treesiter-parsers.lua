201abe10ad1f4caecdd8a38e4226bad2ffe1b789
Luis Zelaya <codewithluis11@gmail.com>
2024-03-24 17:24:20 -0600

    Remove treesiter parsers unused

--- nvim/.config/nvim/lua/config/treesiter-parsers.lua
+++ nvim/.config/nvim/lua/config/treesiter-parsers.lua
@@ -1,14 +1,4 @@
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.php = {
-    install_info = {
-        url = "https://github.com/tree-sitter/tree-sitter-php",
-        files = { "src/parser.c" },
-        branch = "master",
-        generate_requires_npm = false,
-        requires_generate_from_grammar = false,
-    },
-    filetype = "php",
-}
 parser_config.blade = {
     install_info = {
         url = "https://github.com/EmranMR/tree-sitter-blade",
@@ -24,14 +14,6 @@ parser_config.tex = {
         branch = "master"
     },
     filetype = "tex"
-}
-parser_config.markdown = {
-    install_info = {
-        url = 'https://github.com/MDeiml/tree-sitter-markdown',
-        branch = 'split_parser',
-        files = { 'src/parser.c', 'src/scanner.cc' },
-    },
-    filetype = 'markdown'
 }
 
 vim.filetype.add({
