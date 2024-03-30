3ab0a7885434fd8113d2de8026a451689383e4c9
Luis Zelaya <codewithluis11@gmail.com>
2024-03-30 08:25:49 -0600

    Add knap keybindings

--- nvim/.config/nvim/lua/plugins/knap.lua
+++ nvim/.config/nvim/lua/plugins/knap.lua
@@ -1,3 +1,9 @@
 return {
     "frabjous/knap",
+    keys = {
+        vim.keymap.set({ 'n', 'v', 'i' }, '<F5>', function() require("knap").process_once() end),
+        vim.keymap.set({ 'n', 'v', 'i' }, '<F6>', function() require("knap").close_viewer() end),
+        vim.keymap.set({ 'n', 'v', 'i' }, '<F7>', function() require("knap").toggle_autopreviewing() end),
+        vim.keymap.set({ 'n', 'v', 'i' }, '<F8>', function() require("knap").forward_jump() end)
+    }
 }
