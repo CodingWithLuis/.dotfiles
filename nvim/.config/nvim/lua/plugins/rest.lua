f9657aa9f293fa2595303fabb9cf7815f97d9e31
Luis Zelaya <codewithluis11@gmail.com>
2024-03-30 08:33:31 -0600

    change rest config

--- nvim/.config/nvim/lua/plugins/rest.lua
+++ nvim/.config/nvim/lua/plugins/rest.lua
@@ -11,17 +11,13 @@ return {
         "rest-nvim/rest.nvim",
         ft = "http",
         dependencies = { "luarocks.nvim" },
-        config = function()
-            require("rest-nvim").setup({
-                keybinds = {
-                    {
-                        "<leader>rr", "<cmd>Rest run<cr>", "Run request under the cursor",
-                    },
-                    {
-                        "<leader>rl", "<cmd>Rest run last<cr>", "Re-run latest request",
-                    },
-                }
-            })
-        end,
+        keys = {
+            {
+                "<leader>rr", "<cmd>Rest run<cr>", "Run request under the cursor",
+            },
+            {
+                "<leader>rl", "<cmd>Rest run last<cr>", "Re-run latest request",
+            },
+        }
     }
 }
