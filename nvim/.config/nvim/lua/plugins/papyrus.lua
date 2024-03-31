ba8ea05a1896943c9c144d11ef56af5cd9809caa
Luis Zelaya <codewithluis11@gmail.com>
2024-03-30 23:23:23 -0600

    Change default pdf viewer in papyrus

--- nvim/.config/nvim/lua/plugins/papyrus.lua
+++ nvim/.config/nvim/lua/plugins/papyrus.lua
@@ -3,5 +3,6 @@ return {
     keys = {
         { "<leader>pcp", "<CMD>PapyrusCompile pdf --template eisvogel --highlight-style tango --number-sections<CR>", desc = "Compile to PDF using eisvogel template" }
     },
+    { "<leader>pv", "<CMD>PapyrusView<CR>", desc = "Papyrus PDF Viewer" },
     ft = { 'tex', 'markdown' },
 }
