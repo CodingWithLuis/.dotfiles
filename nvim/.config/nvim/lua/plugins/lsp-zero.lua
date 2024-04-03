ad2f499f9a4484dc3b6261711cb76cd6c437423f
Luis Zelaya <codewithluis11@gmail.com>
2024-04-02 21:32:47 -0600

    Add cmd-cmdline plugin and setup

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -161,6 +161,27 @@ return {
             Variable = ' '
         }
 
+        cmp.setup.cmdline('/', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'buffer' }
+            }
+        })
+
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' }
+            }, {
+                {
+                    name = 'cmdline',
+                    option = {
+                        ignore_cmds = { 'Man', '!' }
+                    }
+                }
+            })
+        })
+
         cmp.setup({
             snippet = {
                 expand = function(args)
