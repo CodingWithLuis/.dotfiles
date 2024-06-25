4bc13eee9784a0660870363df6a9ac460aacb6b0
Luis Zelaya <codewithluis11@gmail.com>
2024-06-25 09:33:02 -0600

    Fix issues on lspsaga config

--- nvim/.config/nvim/lua/plugins/lspsaga.lua
+++ nvim/.config/nvim/lua/plugins/lspsaga.lua
@@ -1,10 +1,10 @@
 return {
     'nvimdev/lspsaga.nvim',
     keys = {
-        { "<leader>ca", "<cmd>Lspsaga code_action<cr>",         desc = "Code Action" },
-        { "<leader>gr", "<cmd>Lspsaga rename<cr>",              desc = "Lspsaga Rename" },
-        { "<C-s>d",     "<cmd>Lspsaga term_toggle lazygit<CR>", desc = true },
-        { "<leader>o",  "<cmd>Lspsaga outline<cr>",             desc = "Lspsaga Outline" },
+        { "<leader>ca", ":Lspsaga code_action<cr>" },
+        { "<leader>gr", "<cmd>Lspsaga rename<cr>" },
+        { "<C-s>d",     "<cmd>Lspsaga term_toggle lazygit<CR>" },
+        { "<leader>o",  "<cmd>Lspsaga outline<cr>" },
     },
     event = { "VeryLazy" },
     opts = {
