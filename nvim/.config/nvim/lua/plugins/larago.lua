90b5ce9bb43cab82521537dbaed27111dcfed56b
Luis Zelaya <codewithluis11@gmail.com>
2024-06-28 21:02:24 -0600

    Removing Larago plugin

--- nvim/.config/nvim/lua/plugins/larago.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'ccaglak/larago.nvim',
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
-    keys = {
-        { "<leader>gg", "<cmd>GoBlade<cr>", mode = "n", desc = "Go to Blade View" }
-    }
-}
