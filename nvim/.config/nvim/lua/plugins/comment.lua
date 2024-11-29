983aa23f947f502ef936fa51c45ba4f2733d8392
Luis Zelaya <codewithluis11@gmail.com>
2024-11-29 14:23:33 -0600

    Change config for comment

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,5 +1,8 @@
 return {
     "numToStr/Comment.nvim",
-    keys = { { "gcc", desc = "Comment current line" }, { "gc", mode = "v", desc = "Comment selected text" } },
-    config = true,
+    keys = {
+        { "gcc", desc = "Comment current line" },
+        { "gc",  mode = "v",                   desc = "Comment selected text" }
+    },
+    opts = {}
 }
