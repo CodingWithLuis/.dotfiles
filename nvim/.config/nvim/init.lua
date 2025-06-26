410d2473c5b7450ad34818a3411397349377a826
Luis Zelaya <codewithluis11@gmail.com>
2025-06-26 10:04:22 -0600

    Improving initilize LSP

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -16,16 +16,25 @@ vim.g.papyrus_latex_engine = 'pdflatex'
 
 vim.lsp.enable('luals')
 vim.lsp.enable('marksman')
+
+local intelephense_config = require('lsp.intelephense')
+vim.lsp.config.intelephense = intelephense_config
 vim.lsp.enable('intelephense')
+
 vim.lsp.enable('phpactor')
 vim.lsp.enable('jsonls')
 vim.lsp.enable('texlab')
 vim.lsp.enable('tailwindcss')
 vim.lsp.enable('ts_ls')
 vim.lsp.enable('vue_ls')
+
+local emmet_config = require('lsp.emmetls')
+vim.lsp.config.emmet_ls = emmet_config
 vim.lsp.enable('emmet_ls')
+
 vim.lsp.enable('csharp_ls')
 vim.lsp.enable('eslint')
+vim.lsp.enable('laravel_ls')
 
 require("lazy").setup("plugins")
 require("mappings.general")
