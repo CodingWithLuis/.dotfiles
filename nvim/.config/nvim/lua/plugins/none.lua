28613f35e73247487940bc4276820780f4693f08
Luis Zelaya <codewithluis11@gmail.com>
2025-06-26 22:57:48 -0600

    Removing old syntax to none_ls

--- nvim/.config/nvim/lua/plugins/none.lua
+++ nvim/.config/nvim/lua/plugins/none.lua
@@ -1,26 +1,9 @@
 return {
     'nvimtools/none-ls.nvim',
     config = function()
-        local lsp = require('lsp-zero')
         local null_ls = require('null-ls')
-        null_opts = lsp.build_options('null-ls', {})
 
         null_ls.setup({
-            on_attach = function(client, bufnr)
-                null_opts.on_attach(client, bufnr)
-
-                if client.server_capabilities.documentFormattingProvider then
-                    vim.cmd(
-                        "nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.format({async = true, timeout_ms = 10000})<CR>")
-                    vim.cmd(
-                        "autocmd BufWritePost <buffer> lua vim.lsp.buf.format({async = true, timeout_ms = 10000})")
-                end
-
-                if client.server_capabilities.documentRangeFormattingProvider then
-                    vim.cmd(
-                        "xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.format({async= true, timeout_ms = 10000})<CR>")
-                end
-            end,
             sources = {
                 null_ls.builtins.diagnostics.markdownlint,
                 null_ls.builtins.formatting.markdownlint,
