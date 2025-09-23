78623899c791042ce63d9bfb609f2bec730e2f8b
Luis Zelaya <codewithluis11@gmail.com>
2025-09-23 11:06:36 -0600

    Modified Laravel ls

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -78,6 +78,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
         local opts = { buffer = event.buf }
 
         vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set("n", "gD", '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set("n", "gi", '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set("n", "grr", '<cmd>lua vim.lsp.buf.references()<cr>', opts)
         vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
     end,
--- nvim/.config/nvim/lua/lsp/laravells.lua
+++ nvim/.config/nvim/lua/lsp/laravells.lua
@@ -4,7 +4,7 @@ return {
     root_markers = { 'composer.json', 'artisan' },
     on_attach = function(client, bufnr)
         local opts = { buffer = bufnr, silent = true }
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     end
 }
