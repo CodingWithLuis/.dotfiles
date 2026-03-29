78c104795793944c703b68c5e75316a192c95cf4
Luis Zelaya <codingwithluis@github.com>
2026-03-28 22:02:57 -0600

    Added vue plugins

---
+++ nvim/.config/nvim/lua/lsp/vtsls.lua
@@ -0,0 +1,41 @@
+local vue_language_server_path = vim.fn.stdpath('data') ..
+    '/mason/packages/vue-language-server/node_modules/@vue/language-server'
+
+local vue_plugin = {
+    name = '@vue/typescript-plugin',
+    location = vue_language_server_path,
+    languages = { 'vue' },
+    configNamespace = 'typescript',
+}
+
+return {
+    cmd = { 'vtsls', '--stdio' },
+    filetypes = {
+        'javascript',
+        'javascriptreact',
+        'javascript.jsx',
+        'typescript',
+        'typescriptreact',
+        'typescript.tsx',
+        'vue'
+    },
+    root_markers = { 'package.json', 'tsconfig.json', 'jsconfig.json', '.git' },
+    settings = {
+        vtsls = {
+            tsserver = {
+                globalPlugins = {
+                    vue_plugin,
+                },
+            },
+            autoUseWorkspaceTsdk = true,
+        },
+        typescript = {
+            updateImportsOnFileMove = { enabled = 'always' },
+            inlayHints = {
+                parameterNames = { enabled = 'literals' },
+                parameterTypes = { enabled = true },
+                variableTypes = { enabled = true },
+            },
+        },
+    },
+}
---
+++ nvim/.config/nvim/lua/lsp/vue_ls.lua
@@ -0,0 +1,23 @@
+return {
+    settings = {
+        vue = {
+            inlayHints = {
+                destructuredProps = {
+                    enabled = true,
+                },
+                inlineHandlerLoading = {
+                    enabled = true,
+                },
+                missingProps = {
+                    enabled = true,
+                },
+                optionsWrapper = {
+                    enabled = true,
+                },
+                vBindShorthand = {
+                    enabled = true,
+                },
+            },
+        },
+    },
+}
